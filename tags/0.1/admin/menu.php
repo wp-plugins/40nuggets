15046c55bdc1f7258bca1ce5639bc992969ecc75
eliav@40nuggets.com <eliav@40nuggets.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-25 17:26:24 +0000

    rebuilding version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/40nuggets@881851 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.1/admin/menu.php
@@ -0,0 +1,50 @@
+<?php
+
+// 40Nuggets top level menu
+add_action('admin_menu', 'FortyNuggets_create_menu_page');
+function FortyNuggets_create_menu_page() {
+	global $menu;
+    //$menu[] = array('My Menu', 'manage_options', 'http://www.google.com', '40Nuggets', 'open-if-no-js menu-top', '40Nuggets', 'div' );
+	add_menu_page(
+		'40Nuggets Options',		// The title to be displayed on the corresponding page for this menu
+		'40Nuggets',				// The text to be displayed for this actual menu item
+		'administrator',			// Which type of users can see this menu
+		'40Nuggets',				// The unique ID - that is, the slug - for this menu item
+		'',	// The name of the function to call when rendering the menu for this page
+		'http://40nuggets.com/dashboard/images/favicon.ico'
+	);
+} 
+
+//link sub-menus
+add_action('admin_menu' ,'FortyNuggets_create_submenu_exteranl_link' );
+function FortyNuggets_create_submenu_exteranl_link() {
+	//sub-menus
+	global $submenu;
+	$submenu['40Nuggets'][0] = array( 'Manage', 'manage_options' , getURL("home"));
+
+}
+function getURL($target){
+	$plugin = new Fortynuggets_Plugin ();	
+	$options = $plugin->get_options();
+	$page = empty($target) ? "home" : $target;
+	return "https://40nuggets.com/dashboard/{$page}.php?alk={$options->akl}";
+}
+
+
+//switch account submenu
+add_action('admin_menu', 'FortyNuggets_create_submenu_page'); 
+function FortyNuggets_create_submenu_page() {
+	add_submenu_page(
+		'40Nuggets',				// Register this submenu with the menu defined above
+		'Switch Account',			// The text to the display in the browser when this menu item is active
+		'Switch Account',			// The text for this menu item
+		'administrator',			// Which type of users can see this menu
+		'40Nuggets-login',			// The unique ID - the slug - for this menu item
+		'FortyNuggets_submenu_login_page_display' 	// The function used to render the menu for this page to the screen
+	);
+}
+function FortyNuggets_submenu_login_page_display() {
+	require_once(dirname(__FILE__) . '/login.php');	
+}
+
+?>
