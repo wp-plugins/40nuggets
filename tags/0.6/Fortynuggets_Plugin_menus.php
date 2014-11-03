53a969a7009d17f323d61e875ee27d6199ce82bc
eliav@40nuggets.com <eliav@40nuggets.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-03 14:26:44 +0000

    tagging v0.6
    git-svn-id: https://plugins.svn.wordpress.org/40nuggets@1018740 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.6/Fortynuggets_Plugin_menus.php
@@ -0,0 +1,55 @@
+<?php
+class Fortynuggets_Plugin_menus{
+	
+	public function show(){
+		$plugin = new Fortynuggets_Plugin();
+		$options = $plugin->get_options();
+		
+		//40Nuggets top level menu
+		$this->main_menu();
+			
+		if (!empty($options->akl)){
+			//link sub-menus
+			$this->sub_menus();
+			//switch account submenu
+			$this->switch_accont();
+		}
+	}
+	
+	private function main_menu() {
+		global $menu;
+		add_menu_page(
+			'40Nuggets Options',		// The title to be displayed on the corresponding page for this menu
+			'40Nuggets',				// The text to be displayed for this actual menu item
+			'administrator',			// Which type of users can see this menu
+			'40Nuggets',				// The unique ID - that is, the slug - for this menu item
+			array(&$this, 'submenu_login_page_display'),							// The name of the function to call when rendering the menu for this page
+			'http://40nuggets.com/dashboard/images/favicon.ico'
+		);
+	} 
+
+	private function sub_menus() {
+		$plugin = new Fortynuggets_Plugin();
+		//sub-menus
+		global $submenu;
+		$submenu['40Nuggets'][0] = array( 'Add New', 'manage_options' , $plugin->getURL("nuggetCreator"));
+		$submenu['40Nuggets'][1] = array( 'Manage Nuggets', 'manage_options' , $plugin->getURL("nuggetManager"));
+		$submenu['40Nuggets'][2] = array( 'Stats', 'manage_options' , $plugin->getURL("home"));
+	}
+
+	private function switch_accont() {
+		add_submenu_page(
+			'40Nuggets',				// Register this submenu with the menu defined above
+			'Switch Account',			// The text to the display in the browser when this menu item is active
+			'Switch Account',			// The text for this menu item
+			'administrator',			// Which type of users can see this menu
+			'40Nuggets-login',			// The unique ID - the slug - for this menu item
+			array(&$this, 'submenu_login_page_display') 	// The function used to render the menu for this page to the screen
+		);
+	}
+	
+	public function submenu_login_page_display() {
+		require_once ('login.php');	
+	}
+}
+?>
