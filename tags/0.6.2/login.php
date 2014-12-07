ec1e2de9993c3b00b0f11a63281a448f7faa5688
eliav@40nuggets.com <eliav@40nuggets.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-07 09:34:19 +0000

    tagging v0.6.2
    git-svn-id: https://plugins.svn.wordpress.org/40nuggets@1039736 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.6.2/login.php
@@ -0,0 +1,78 @@
+<?php
+//see: http://php.net/manual/en/function.get-magic-quotes-gpc.php
+$form = get_magic_quotes_gpc() ? my_stripslashes_deep($_REQUEST) : $_REQUEST;
+
+if (isset($form['redeem-account'])){
+	//login		
+	if ($plugin->login($form["email"], $form["password"])){
+		$url = $plugin->getURL("home");
+		$url = $plugin->getURL("home");
+		echo "<div id='message' class='updated'>
+			<p align='center'><strong>Your site is now attached to 40Nuggets.</strong></p><p align='center'><a href='{$url}'>Click here to manage your account.</a></p>
+			</div>";
+		exit;
+	}else{
+		echo "<div id='message' class='error'>
+			<p align='center'><strong>Login Failed</strong></p>
+			</div>";
+		showLoginForm($form["email"]);
+	}
+}else{
+	$email = get_option('admin_email');
+	try {
+		$file = plugin_dir_path(__FILE__) . 'fortynuggets.key';
+		if (file_exists($file)){
+			$data = json_decode(file_get_contents($file));
+			$email = $data->email;
+		}
+	}catch(Exception $e) {}
+	showLoginForm($email);
+}
+
+function showLoginForm($email){
+?>
+<div class="wrap">
+	<form method="POST" action="">
+		<h2>Attach your site to an existing account</h2>
+		<p class="description">
+			If you already have an existing account just enter your login and password below.
+		</p>
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row">
+						<label for="email">E-mail</label>
+					</th>
+					<td>
+						<input name="email" type="text" id="email" value="<?php echo $email;?>" class="regular-text code" />
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<label for="password">Password</label>
+					</th>
+					<td>
+						<input name="password" type="password" id="password" value="" class="regular-text code" />
+						<br/>
+						<p class="description"><a href="https://40nuggets.com/site/recovery.php" target="_blank">Forgot your password?</a></p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<p class="submit">
+			<input type="hidden" name="redeem-account" />
+			<input class="button-primary" type="submit" name="login" value=" <?php _e( 'Attach' ); ?> " />
+		</p>
+	</form>
+</div>
+<?php
+}
+
+
+function my_stripslashes_deep($value){
+	$value = is_array($value) ?
+	array_map(array($this, 'stripslashes_deep'), $value) :   
+	stripslashes($value);
+	return $value;
+}
+?>
